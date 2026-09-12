f49e085579991c01f5ea9ccc6a280c2a0031ec9b
MayurMittal01 <mayurmittal59@gmail.com>
2026-09-12 12:30:39 +0530

    Time: 1 ms (99.92%), Space: 77.2 MB (66.21%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -5,7 +5,8 @@ class Solution {
         int maxSum = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            currentSum+=nums[i];
+            currentSum = Math.max(nums[i], currentSum);
             maxSum = Math.max(maxSum, currentSum);
         }
 
